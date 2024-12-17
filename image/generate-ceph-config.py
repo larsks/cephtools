ba108a436b347b5559e1a771da559b22d31c934f
Lars Kellogg-Stedman <lars@oddbit.com>
2024-12-16 22:29:03 -0500

    Better parsing of the cluster details file

--- image/generate-ceph-config.py
+++ image/generate-ceph-config.py
@@ -1,10 +1,10 @@
 import argparse
-import json
 import os
 
-from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
+
+from pydantic import BaseModel, RootModel, model_validator
 
 default_credentials_file = os.getenv(
     "CEPH_CREDENTIALS_FILE", "/ceph/external_cluster_details"
@@ -13,10 +13,25 @@ default_credentials_file = os.getenv(
 default_config_dir = os.getenv("CEPH_CONFIG_DIR", "/etc/ceph")
 
 
-@dataclass
-class Args:
+class Args(BaseModel):
     credentials_file: Path = Path(default_credentials_file)
     config_dir: Path = Path(default_config_dir)
+
+class Detail(BaseModel):
+    name: str
+    kind: str
+    data: dict[str, str] = {}
+
+class Details(RootModel[dict[str, Detail]]):
+    root: dict[str, Detail]
+
+    @model_validator(mode='before')
+    @classmethod
+    def list_to_dict(cls, data: dict[str, Any]|list[dict[str, Any]]) -> dict[str, Any]:
+        if isinstance(data, list):
+            return {item['name']: item for item in data}
+
+        return data
 
 
 def parse_args() -> Args:
@@ -39,11 +54,11 @@ def parse_args() -> Args:
     return Args(**vars(p.parse_args()))
 
 
-def write_users(creds: list[dict[str, Any]], configdir: Path):
-    for item in creds:
-        if item["kind"] == "Secret" and "userID" in item.get("data", {}):
-            userid = item["data"]["userID"]
-            userkey = item["data"]["userKey"]
+def write_users(details: dict[str, Detail], configdir: Path):
+    for item in details.values():
+        if item.kind == "Secret" and "userID" in item.data:
+            userid = item.data["userID"]
+            userkey = item.data["userKey"]
             if not userid.startswith("client."):
                 userid = f"client.{userid}"
 
@@ -52,9 +67,9 @@ def write_users(creds: list[dict[str, Any]], configdir: Path):
                 fd.write(f"key={userkey}\n")
 
 
-def write_ceph_conf(creds: list[dict[str, Any]], configdir: Path):
+def write_ceph_conf(details: dict[str, Detail], configdir: Path):
     mon_host = (
-        next(x for x in creds if x["name"] == "rook-ceph-mon-endpoints")["data"]["data"]
+        details['rook-ceph-mon-endpoints'].data['data']
         .splitlines()[0]
         .split("=", 1)[1]
     )
@@ -64,11 +79,10 @@ def write_ceph_conf(creds: list[dict[str, Any]], configdir: Path):
         fd.write(f"mon_host={mon_host}\n")
 
 
-def write_shell_vars(creds: list[dict[str, Any]], configdir: Path):
-    pool = next(x for x in creds if x["name"] == "ceph-rbd")["data"]["pool"]
-    provisioner = next(x for x in creds if x["name"] == "rook-csi-rbd-provisioner")[
-        "data"
-    ]["userID"]
+def write_shell_vars(details: dict[str, Detail], configdir: Path):
+    pool = details['ceph-rbd'].data['pool']
+    provisioner = details['rook-csi-rbd-provisioner'].data['userID']
+
     with (configdir / "ceph.env").open("w") as fd:
         fd.write(f'export CEPH_ARGS="--user {provisioner}"\n')
         fd.write(f'export RBD_POOL="{pool}"\n')
@@ -78,11 +92,11 @@ def main():
     args = parse_args()
 
     with open(args.credentials_file) as fd:
-        creds = json.load(fd)
+        details = Details.model_validate_json(fd.read())
 
-    write_users(creds, args.config_dir)
-    write_ceph_conf(creds, args.config_dir)
-    write_shell_vars(creds, args.config_dir)
+    write_users(details.root, args.config_dir)
+    write_ceph_conf(details.root, args.config_dir)
+    write_shell_vars(details.root, args.config_dir)
 
 
 if __name__ == "__main__":
